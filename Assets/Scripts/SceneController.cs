250c06fbb9d258717537cd58cee40a49ab198f95
arena-allocator-abuser <rogopa6540@degcos.com>
2025-02-03 10:40:58 +0100

    SceneController.SceneIsLoaded()

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -11,7 +11,36 @@ public class SceneController : MonoBehaviour {
 		for (int i = 0; i < loadOnStartup.Length; ++i) {
 			SceneManager.LoadScene(loadOnStartup[i].name, LoadSceneMode.Additive);
 		}
+
+		if (SceneIsLoaded("BasicScene")) {
+			print("BasicScene is loaded!");
+		}
 	}
+
+	public bool SceneIsLoaded(int buildIndex) {
+		for (int i = 0; i < SceneManager.sceneCount; ++i) {
+			var scene = SceneManager.GetSceneAt(i);
+			if (scene.buildIndex < 0) {
+				Debug.LogError($"Scene {scene.name} does is not in the build!");
+			} else if (scene.buildIndex == buildIndex) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public bool SceneIsLoaded(string name) {
+		return SceneIsLoaded(SceneManager.GetSceneByName(name).buildIndex);
+	}
+
+	/*
+	public void LoadScene(SceneDings dings) {
+		// if (dings is not loaded) {
+		SceneManager.LoadScene(, LoadSceneMode.Additive);
+		// }
+	}
+	*/
 
 	public void ReloadAllScenes() {
 		// By reloading the scene controller as the only scene, we
