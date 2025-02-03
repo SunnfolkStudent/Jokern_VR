d52eca5cdec1655101ddc23ac8ae2c57edc8ffd2
arena-allocator-abuser <rogopa6540@degcos.com>
2025-02-03 11:16:36 +0100

    Some useful scene loading and unloading functions

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,9 +1,11 @@
+using System.Collections;
 using UnityEngine;
 using UnityEditor;
 using UnityEngine.SceneManagement;
 using static AugustBase.All;
 
 public class SceneController : MonoBehaviour {
+	// TODO: This may break on build since we are using something from UnityEditor.
 	[Tooltip("These scenes will get loaded additively.")]
 	public SceneAsset[] loadOnStartup;
 
@@ -11,17 +13,21 @@ public class SceneController : MonoBehaviour {
 		for (int i = 0; i < loadOnStartup.Length; ++i) {
 			SceneManager.LoadScene(loadOnStartup[i].name, LoadSceneMode.Additive);
 		}
+	}
 
+	void Start() {
 		if (SceneIsLoaded("BasicScene")) {
 			print("BasicScene is loaded!");
 		}
+
+		LoadSceneIfNotAlreadyLoaded("BasicScene");
 	}
 
 	public bool SceneIsLoaded(int buildIndex) {
 		for (int i = 0; i < SceneManager.sceneCount; ++i) {
 			var scene = SceneManager.GetSceneAt(i);
 			if (scene.buildIndex < 0) {
-				Debug.LogError($"Scene {scene.name} does is not in the build!");
+				Debug.LogError($"Scene '{scene.name}' is not in the build!");
 			} else if (scene.buildIndex == buildIndex) {
 				return true;
 			}
@@ -31,16 +37,36 @@ public class SceneController : MonoBehaviour {
 	}
 
 	public bool SceneIsLoaded(string name) {
-		return SceneIsLoaded(SceneManager.GetSceneByName(name).buildIndex);
+		return SceneIsLoaded(SceneNameToBuildIndex(name));
 	}
 
-	/*
-	public void LoadScene(SceneDings dings) {
-		// if (dings is not loaded) {
-		SceneManager.LoadScene(, LoadSceneMode.Additive);
-		// }
+	public void LoadSceneIfNotAlreadyLoaded(int buildIndex) {
+		if (!SceneIsLoaded(buildIndex)) {
+			LoadScene(buildIndex);
+		}
 	}
-	*/
+
+	public void LoadSceneIfNotAlreadyLoaded(string name) {
+		if (!SceneIsLoaded(name)) {
+			LoadScene(name);
+		}
+	}
+
+	// NOTE: These functions may not work in Awake().
+	public void UnloadScene(int buildIndex) => SceneManager.UnloadSceneAsync(buildIndex);
+	public void UnloadScene(string name)    => SceneManager.UnloadSceneAsync(name);
+
+	public int SceneNameToBuildIndex(string name) {
+		return SceneManager.GetSceneByName(name).buildIndex;
+	}
+
+	public void LoadScene(string name) {
+		SceneManager.LoadScene(name, LoadSceneMode.Additive);
+	}
+
+	public void LoadScene(int buildIndex) {
+		SceneManager.LoadScene(buildIndex, LoadSceneMode.Additive);
+	}
 
 	public void ReloadAllScenes() {
 		// By reloading the scene controller as the only scene, we
