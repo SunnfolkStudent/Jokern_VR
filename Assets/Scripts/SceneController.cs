ba4378816271691901037580aa725ce2963c8ac2
arena-allocator-abuser <rogopa6540@degcos.com>
2025-02-03 10:21:11 +0100

    Load specified scenes on startup

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,8 +1,22 @@
 using UnityEngine;
+using UnityEditor;
 using UnityEngine.SceneManagement;
+using static AugustBase.All;
 
 public class SceneController : MonoBehaviour {
+	[Tooltip("These scenes will get loaded additively.")]
+	public SceneAsset[] loadOnStartup;
+
 	void Awake() {
-		print("How scenic!");
+		for (int i = 0; i < loadOnStartup.Length; ++i) {
+			SceneManager.LoadScene(loadOnStartup[i].name, LoadSceneMode.Additive);
+		}
+	}
+
+	public void ReloadAllScenes() {
+		// By reloading the scene controller as the only scene, we
+		// effectively reload everything since it is responsible for
+		// setting up all the other scenes.
+		SceneManager.LoadScene("SceneController", LoadSceneMode.Single);
 	}
 }
