5f2473dcf1d0df8f7d3f7536620e0776abebf3e8
arena-allocator-abuser <rogopa6540@degcos.com>
2025-02-05 10:09:53 +0100

    Remove 'name' field due to annoying warning

--- Assets/Scripts/IK/IKPhysicsRig.cs
+++ Assets/Scripts/IK/IKPhysicsRig.cs
@@ -9,16 +9,12 @@ public class IKPhysicsRig : MonoBehaviour
     [SerializeField, Tooltip("Displays the target's position in the Inspector")]
     private Vector3 targetPosition;
 
-    [SerializeField, Tooltip("Displays the target's position in the Inspector")]
-    private String name;
-
     // public ConfigurableJoint headJoint;
     public ConfigurableJoint leftHandJoint;
     public ConfigurableJoint rightHandJoint;
     
     private void FixedUpdate()
     {
-        name = leftController.name;
         targetPosition = leftController.position;
         leftHandJoint.targetPosition = leftController.position;
         leftHandJoint.targetRotation = leftController.rotation;
