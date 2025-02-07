8b181e2088e81159bc4e970dd55cdf891ffefc44
Drianna-79 <sofmolnet@gmail.com>
2025-02-07 13:35:06 +0100

    Flashlight & battery lifetime
    Still missing shaking the flashlight to wake it up, but otherwise functional

--- Assets/Scripts/FlashlightMaster.cs
+++ Assets/Scripts/FlashlightMaster.cs
@@ -2,5 +2,55 @@ using UnityEngine;
 
 public class FlashlightMaster : MonoBehaviour
 {
+    [SerializeField]private float lightTimer;
+    [SerializeField]private float lightIntensity;
+    [SerializeField]private float lightLifetime = 90;
     
+    [SerializeField]private bool isLightOn = true;
+    private Light light;
+
+    private void Start()
+    {
+        lightTimer = lightLifetime;
+        light = GetComponent<Light>();
+        if (isLightOn == false)
+        {
+            isLightOn = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (isLightOn)
+        {
+            light.enabled = true;
+            lightTimer -= Time.deltaTime;
+            lightIntensity = Mathf.Floor(lightTimer);
+        }
+
+        if (lightTimer > 50) return;
+        if (lightTimer > 1)
+        {
+            light.intensity = lightIntensity; 
+        }
+        else if (lightTimer <= 1 && isLightOn)
+        {
+            light.intensity = Random.Range(0f, 1f);
+        }
+
+        if (lightTimer <= 0 && isLightOn)
+        {
+            isLightOn = false;
+            light.enabled = false;
+        }
+        // if Shake then
+        // { lightTimer = lightLifetime; }
+
+        
+    }
 }
+#region TODO
+//TODO: Check for motion input (shaking), which refills the lifetime
+//TODO: When lifetime is at 90 (100%), re-enable light and continue battery loop
+
+#endregion
