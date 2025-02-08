efb36c471b0f427cff6aa6041639c3116c189f73
arena-allocator-abuser <rogopa6540@degcos.com>
2025-02-08 11:10:52 +0100

    Test!

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,15 +2,35 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+	public Transform playerToMove;
+    void Start() {
+		if (playerToMove == null) {
+			Debug.LogError("No player assigned to the 'Player To Move' field.");
+		}
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public Vector3 velocity;
+	public float moveForce = 100.0f;
+	public float friction = 0.85f;
+	public Vector2 maxMoveSpeed = new Vector2(20.0f, 20.0f); // TODO
+
+	void UpdateMovement(Vector2 inputDirection) {
+		float deltaTime = Time.deltaTime;
+
+		velocity += deltaTime * (moveForce * (
+			  (playerToMove.right   * inputDirection.x)
+			+ (playerToMove.forward * inputDirection.y)
+		));
+
+		playerToMove.position += deltaTime * velocity;
+	}
+
+	void Update() {
+		UpdateMovement(new Vector2(0.0f, 1.0f));
+	}
+
+	void FixedUpdate() {
+		velocity.x *= friction;
+		velocity.z *= friction;
+	}
 }
