1f808e9d5badb6e91902355dad7ae788ff07d233
arena-allocator-abuser <rogopa6540@degcos.com>
2025-02-12 10:55:41 +0100

    Wait for FMOD parameter change when voice lines are over

--- Assets/Scripts/FMOD/FMODController.cs
+++ Assets/Scripts/FMOD/FMODController.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.Events;
 using FMODUnity;
 using FMOD.Studio;
 
@@ -33,6 +34,31 @@ public class FMODController : MonoBehaviour {
 		//footstepSoundInstance.start();
 	}
 
+	const string parameterName_isPlayingVoiceLine = "IsPlayingVoiceLine";
+	static bool weThinkFMODIsPlayingAVoiceLine;
+
+	public static UnityEvent onVoiceLineEnd;
+
+	void FixedUpdate() {
+		if (weThinkFMODIsPlayingAVoiceLine) {
+			float isPlayingVoiceLineAsFloat;
+			var fmodStatus = RuntimeManager.StudioSystem.getParameterByName(parameterName_isPlayingVoiceLine,
+			                                                                out isPlayingVoiceLineAsFloat);
+			if (fmodStatus == FMOD.RESULT.OK) {
+				bool isPlayingVoiceLine = isPlayingVoiceLineAsFloat != 0.0f;
+				if (!isPlayingVoiceLine) {
+					weThinkFMODIsPlayingAVoiceLine = false;
+
+					if (onVoiceLineEnd != null) {
+						onVoiceLineEnd.Invoke();
+					}
+				}
+			} else {
+				Debug.LogError($"FMOD is not ok! ({fmodStatus.ToString()})");
+			}
+		}
+	}
+
 	public static void PlaySound(JokernVRSound sound) {
 		if (sound == JokernVRSound.None) return;
 
@@ -58,6 +84,8 @@ public class FMODController : MonoBehaviour {
 	}
 
 	public static void PlayVoiceLine(string path) {
+		RuntimeManager.StudioSystem.setParameterByName(parameterName_isPlayingVoiceLine, 1.0f);
 		RuntimeManager.PlayOneShot(path);
+		weThinkFMODIsPlayingAVoiceLine = true;
 	}
 }
