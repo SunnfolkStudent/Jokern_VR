0650018198bba4a1fbc0f6117348854f3dd0b4fc
WiDuck1 <87336958+WiDuck1@users.noreply.github.com>
2025-02-12 14:42:40 +0100

    Moved menu scripts to their new home

--- Assets/Scripts/Menus/MovementBoolScript.cs
+++ Assets/Scripts/Menus/MovementBoolScript.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class MovementBoolScript : MonoBehaviour
 {
+    // This script is used for changing the method of locomotion in both the main menu and pause menu 
     public void MovementBool()
     {
         if (PlayerMovement.isUsingStickMovement == true)
