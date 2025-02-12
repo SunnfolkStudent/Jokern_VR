67f2f7d231b3ddee3cba051e0a71b17fc145cdf2
Trym-br <trym.brandal.arhaug@gmail.com>
2025-02-12 15:06:56 +0100

    BackupPortal Progess

--- Assets/PortalController.cs
+++ Assets/PortalController.cs
@@ -5,13 +5,17 @@ public class PortalController : MonoBehaviour
 {
     [FormerlySerializedAs("Other")] public Transform OtherPortal;
     public Vector3 Offset;
+    public Transform PlayerTransform;
     private float RotY;
 
     private void OnTriggerEnter(Collider probablyPlayer)
     {
+        print("Collided with " + probablyPlayer.name);
         if (!probablyPlayer.CompareTag("Player")) return;
+        print("HIT PLAYER");
         
-        probablyPlayer.transform.position = OtherPortal.position;
+        // PlayerTransform = probablyPlayer.gameObject.GetComponent<PlayerMover>().GetTargetReference();
+        UpdatePosition(probablyPlayer);
         
         UpdateRotation(probablyPlayer);
     }
@@ -30,5 +34,17 @@ public class PortalController : MonoBehaviour
         var rotXZ = probablyPlayer.transform.eulerAngles;
         
         probablyPlayer.transform.localEulerAngles = new Vector3(rotXZ.x, RotY, rotXZ.z);
+    }
+
+    private void UpdatePosition(Collider probablyPlayer)
+    {
+        print("PlayerPos Before: " + probablyPlayer.transform.position);
+        // probablyPlayer.transform.position = OtherPortal.position;
+        Vector3 otherPosOffset = transform.position - probablyPlayer.transform.position;
+        otherPosOffset.y = 0;
+        // probablyPlayer.transform.position = OtherPortal.position + otherPosOffset;
+        probablyPlayer.GetComponent<PlayerMover>().Move(OtherPortal.position + otherPosOffset);
+        print("PlayerPos After: " + probablyPlayer.transform.position);
+        print("PlayerPos Target: " + (OtherPortal.position + otherPosOffset));
     }
 }
