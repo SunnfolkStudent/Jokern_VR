4474a81189832706700854f899aa3ceb4b4dea3e
arena-allocator-abuser <rogopa6540@degcos.com>
2025-02-18 09:54:51 +0100

    For erik

--- Assets/Scripts/Scene Controller/SceneControllerAccesser.cs
+++ Assets/Scripts/Scene Controller/SceneControllerAccesser.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 
 public class SceneControllerAccesser : MonoBehaviour {
-	public static void CloseMainMenu() => SceneController.CloseMainMenu();
-	public static void OpenMainMenu()  => SceneController.OpenMainMenu();
+	public static void CloseMainMenu()     => SceneController.CloseMainMenu();
+	public static void OpenMainMenu()      => SceneController.OpenMainMenu();
+	public static void QuitEverything()    => SceneController.QuitEverything();
+	public static void LoadNextLevel()     => SceneController.LoadNextLevel();
+	public static void LoadPreviousLevel() => SceneController.LoadPreviousLevel();
 }
