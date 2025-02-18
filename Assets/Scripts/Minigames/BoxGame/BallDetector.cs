65d5c575da5192395868a404d573bf34920bf3bf
Trym-br <trym.brandal.arhaug@gmail.com>
2025-02-18 09:58:22 +0100

    MiniGame Code Changes

--- Assets/Scripts/Minigames/BoxGame/BallDetector.cs
+++ Assets/Scripts/Minigames/BoxGame/BallDetector.cs
@@ -9,12 +9,15 @@ public class BallDetector : MonoBehaviour
     private Vector3 _ballPosition;
     private Quaternion _ballRotation;
 
+    public bool isUsed;
+
     private void Start()
     {
         _ball = GetComponent<Transform>(); //gets the position of the ball
         _ballPosition = _ball.position; //saves the balls position so it can return on reset
         _ballRotation = _ball.rotation; //saves the balls rotation so it can return on reset this is semi unnecessary
         _rigidbody = GetComponent<Rigidbody>(); //gets the rigidbody of the ball
+        isUsed = false;
     }
     
     public void ResetTransform() //a method that makes it possible for the boxGameManager to reset the ball back to start pos
@@ -23,5 +26,6 @@ public class BallDetector : MonoBehaviour
         _ball.rotation = _ballRotation;
         _rigidbody.linearVelocity = new Vector3(0, 0, 0);
         _rigidbody.angularVelocity = new Vector3(0, 0, 0);
+        isUsed = false;
     }
 }
