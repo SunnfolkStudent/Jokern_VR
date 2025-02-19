af8fbd782419b990b499527cc9c296d3a3031899
Trym-br <trym.brandal.arhaug@gmail.com>
2025-02-19 21:29:11 +0100

    Made Mini games

--- Assets/Scripts/Misc/c4.cs
+++ Assets/Scripts/Misc/c4.cs
@@ -6,6 +6,7 @@ public class c4 : MonoBehaviour
     [SerializeField] private GameObject confettiPSPrefab;
     // private ParticleSystem confettiSystem;
     private float confettiTime = 3f;
+    private BoxCollider collider;
     private MeshRenderer objectRenderer;
     private Vector3 centerPos;
 
@@ -13,11 +14,13 @@ public class c4 : MonoBehaviour
     {
         // Instantiate(confettiPSPrefab, transform.position, Quaternion.identity);
         objectRenderer = this.gameObject.GetComponent<MeshRenderer>();
+        collider = this.gameObject.GetComponent<BoxCollider>();
     }
 
     public void Explode()
     {
         objectRenderer.enabled = false;
+        collider.enabled = false;
         centerPos = objectRenderer.bounds.center;
         GameObject confettiInstance =  Instantiate(confettiPSPrefab, centerPos, Quaternion.identity);
         // ParticleSystem confettiInstance = Instantiate(confettiSystem, transform.position, Quaternion.identity);
